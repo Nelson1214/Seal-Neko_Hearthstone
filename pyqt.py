3899790a78ac840ca7ab2d2b25455e9656fded69
kevin77688 <kevin77688@gmail.com>
2021-01-15 01:24:46 +0800

    Update View

--- pyqt.py
+++ pyqt.py
@@ -7,35 +7,51 @@ from PyQt5.QtWidgets import (
     QWidget,
     QVBoxLayout,
     QScrollArea,
+    QLabel,
 )
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import Qt
 
 class Window(QWidget):
     def __init__(self, heroList):
         super().__init__()
         self.setWindowTitle("HearthStone")
+        self.heroList = heroList
         self.heroCount = len(heroList)
-        mainLayout = QHBoxLayout()
-        self.setLeftPanel(mainLayout)
+        self.mainLayout = QHBoxLayout()
+        self.setLeftPanel()
+        self.setRightPanel()
+        self.setLayout(self.mainLayout)
         
-    def setLeftPanel(self, mainLayout):
+    def setLeftPanel(self):
+        heroScroll = QScrollArea()
         heroVertical = QVBoxLayout()
-        widthCount = int(self.heroCount % 3)
-        heightCount = int((self.heroCount - widthCount) / 3)
-        print(heightCount)
-        for i in range(heightCount):
-            heroHorizonal = QHBoxLayout()
-            for j in range(3):
-                heroButton = QPushButton(str(3 * i + j))
-                heroHorizonal.addWidget(heroButton)
-            heroVertical.addLayout(heroHorizonal)
-        mainLayout.addLayout(heroVertical)
         heroHorizonal = QHBoxLayout()
-        heroButton = QPushButton('')
-        heroHorizonal.addWidget(heroButton)
-        heroHorizonal.addStretch(2)
-        heroVertical.addLayout(heroHorizonal)
-        # Set the layout on the application's window
-        self.setLayout(mainLayout)
+        for index, hero in enumerate(self.heroList):
+            if (index % 3 == 0):
+                heroHorizonal = QHBoxLayout()
+                heroVertical.addLayout(heroHorizonal)
+            heroButton = QPushButton(hero.get_name())
+            heroButton.clicked.connect(self.setDetail(hero.get_id()))
+            heroButton.setIcon(QIcon("./hs/" + str(hero.get_id()) + ".png")) 
+            heroHorizonal.addWidget(heroButton)
+        heroScroll.setLayout(heroVertical)
+        heroScroll.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        heroScroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        heroScroll.setWidgetResizable(False)
+        heroScroll.setFixedWidth(400)
+        self.mainLayout.addWidget(heroScroll)
+
+    def setRightPanel(self):
+        dataVertical = QVBoxLayout()
+        self.heroNameLabel = QLabel("Name : ")
+        dataVertical.addWidget(self.heroNameLabel)
+        self.mainLayout.addLayout(dataVertical)
+
+    def setDetail(self, hero_id):
+        # self.heroNameLabel.setText("Name" + str(hero_id))
+        return
+
 
 def createView(heroList):
     app = QApplication(sys.argv)
