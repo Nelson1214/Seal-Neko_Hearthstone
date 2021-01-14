63b3931406d3f32d10289bbf12d1aba6c7edabdb
Nelson1214 <t106820018@ntut.org.tw>
2021-01-14 17:30:00 +0800

    added main?

--- jsonParse.py
+++ jsonParse.py
@@ -1,22 +1,30 @@
 import json
 
-battle_grounds_hero_lst=[]
-id_to_name_lst=[]
-keys_lst=["dbfId", "name"]
+def id_to_name():
+    battle_grounds_hero_lst=[]
+    id_to_name_lst=[]
+    keys_lst=["dbfId", "name"]
 
-f = open('all.json', encoding="utf-8") 
-data = json.load(f)
-f.close()
+    f = open('all.json', encoding="utf-8") 
+    data = json.load(f)
+    f.close()
 
-for i in data:
-    if "battlegroundsHero" in i:
-        battle_grounds_hero_lst.append(i)
+    for i in data:
+        if "battlegroundsHero" in i:
+            battle_grounds_hero_lst.append(i)
 
-for dict_1 in battle_grounds_hero_lst:
-    dict_2 = dict_1.copy()
-    for keys in dict_1.keys():
-        if keys not in keys_lst:
-            dict_2.pop(keys)
-    id_to_name_lst.append(dict_2)
+    for dict_1 in battle_grounds_hero_lst:
+        dict_2 = dict_1.copy()
+        for keys in dict_1.keys():
+            if keys not in keys_lst:
+                dict_2.pop(keys)
+        id_to_name_lst.append(dict_2)
 
-print(id_to_name_lst)
+    return(id_to_name_lst)
+
+def winrate():
+    f = open('battleGround.json', encoding="utf-8") 
+    data = json.load(f)
+    f.close()
+
+    
---
+++ main.py
@@ -0,0 +1,4 @@
+import jsonParse
+
+if __name__ == "__main__":
+    print(jsonParse.id_to_name())
